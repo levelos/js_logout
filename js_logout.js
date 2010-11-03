363e56ab01ce88e022c06f0d06bb1d11ba2cf6da
Lev Tsypin <lev@levelos.com>
2010-11-03 15:12:49 -0700

    implement server side fallback

--- js_logout.js
+++ js_logout.js
@@ -5,10 +5,19 @@ Drupal.behaviors.js_logout = function (context) {
   
   function logout() {
     if (confirm(Drupal.settings.js_logout.message) ) {
-      t = setTimeout(logout, Drupal.settings.js_logout.timeout);
+      $.ajax({
+        url: Drupal.settings.basePath + 'js_logout_ahah_set_last',
+        type: "POST",
+        success: function() {
+          t = setTimeout(logout, Drupal.settings.js_logout.timeout);
+        },
+        error: function() {
+          alert('There has been an error resetting your last access time.')
+        },
+      });      
     } else {
       $.ajax({
-        url: Drupal.settings.basePath + 'js_logout',
+        url: Drupal.settings.basePath + 'js_logout_ahah_logout',
         type: "POST",
         success: function() {
           document.location.href = Drupal.settings.js_logout.redirect_url;
